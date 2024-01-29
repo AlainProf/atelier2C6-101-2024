ef96e139f930d00308d0b84693651311622ccee9
AlainProf <amartel@cstj.qc.ca>
2024-01-29 16:39:37 -0500

    structure de menu

--- Program.cs
+++ Program.cs
@@ -2,70 +2,55 @@
 {
     internal class Program
     {
-        static double detteInitiale;
         static void Main(string[] args)
         {
-            Console.Clear();
-            Console.WriteLine("\n\n\n__________________________________\nLe Financier");
-
-            saisirParametres();
-
-            double tauxInteretAnnuel = 0.21;
-            double tauxInteretMensuel = tauxInteretAnnuel / 12;
-
-            Console.WriteLine("Dette initiale:" + detteInitiale + "$\nTaux intérêt annuel:" + tauxInteretAnnuel);
-
             bool go = true;
-            int mois = 1;
-
-            double interetCum = 0;
-            double paiementCum = 0;
-            double solde = detteInitiale;
             while (go)
             {
-                double paiementMin = 0.04 * solde;
-                double interetCourant = solde * tauxInteretMensuel;
-                paiementCum += paiementMin;
-                interetCum += interetCourant;
-
-                if (mois % 10 == 0)
-                {
-                    Console.WriteLine(mois.ToString("N0").PadLeft(6) + " "
-                               + solde.ToString("N2").PadLeft(9) + " "
-                               + paiementMin.ToString("N2").PadLeft(7) + " "
-                               + paiementCum.ToString("N2").PadLeft(9) + " "
-                               + interetCourant.ToString("N2").PadLeft(7) + " "
-                               + interetCum.ToString("N2").PadLeft(9));
-                }
-                solde -= paiementMin - interetCourant;
-                mois++;
-
-
-
-                if (solde < 1)
-                    go = false;
+                util.titre("Atelier 2C6 gr 101\n__________________________________");
+                AfficherMenu();
+                ExecuterChoix();
             }
         }
 
-        static void saisirParametres()
+
+        static void ExecuterChoix()
         {
-            detteInitiale = saisirUnDouble("Montant de la dette");
-            //Console.WriteLine("Votre dette saisie:" + dette + "$");
+            char choix = util.SaisirChar();
+            string option = choix.ToString().ToUpper();
 
-        }
-
-        static double saisirUnDouble(string nomDuDouble)
-        {
-            Console.WriteLine(nomDuDouble + ":");
-            string input = Console.ReadLine();
-
-
-            if (double.TryParse(input, out double resultat))
+            switch(option)
             {
-                return resultat;
+                case ("F"):
+                    ExecFinancier();
+                    break;
+                case ("H"):
+                    ExecHumanite();
+                    break;
+                case ("Q"):
+                default:
+                    Environment.Exit(0);
+                    break;
             }
-            else
-                return 0.0;
         }
+        static void ExecFinancier()
+        {
+            financier fin = new financier();
+            fin.exec();
+        }
+        static void ExecHumanite()
+        {
+            Console.WriteLine("Humanité");
+            util.pause();
+        }
+
+        static void AfficherMenu()
+        {
+            Console.WriteLine("f- Financier");
+            Console.WriteLine("h- Humanité");
+            Console.WriteLine("q- Quitter");
+            Console.Write("\nvotre choix:");
+        }
+
     }
 }
